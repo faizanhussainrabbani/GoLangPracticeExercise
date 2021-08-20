232dcc05cde7886b4232ec6baec8d90117d3809a
Faizan Hussain Rabbani <fezarfz@gmail.com>
2021-08-20 17:08:36 +0500

    Added allarticles API

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json: Title`
+	Desc    string `json: desc`
+	Content string `json: Content`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Title 1", Desc: "Desc 1", Content: "Content 1"},
+	}
+	fmt.Println("All articles hit")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage hit")
@@ -12,6 +29,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/allarticles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
